488f2470ccb87b93f647c01eea8709ac1efd0da7
Giorgio Ruitón Mauricci <gpaoloruitonm@gmail.com>
2025-10-23 09:38:41 -0500

    Update some feedback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import HeroSection from '@/components/sections/hero';
 import ContactSection from '@/components/sections/contact';
-import AboutMeSection from '@/components/sections/about-me';
-import SkillsSection from '@/components/sections/skills';
 import ExperienceSection from '@/components/sections/experiences';
-import TestimonialsSection from '@/components/sections/testimonials';
 import WorkSection from '@/components/sections/work';
 
 export default function Home() {
