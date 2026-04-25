d98aafad1fd968668cb7a99bda54c9a6c39501d6
Giorgio Ruitón Mauricci <gpaoloruitonm@gmail.com>
2026-04-25 15:15:23 -0500

    Update archive cv

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/general/button';
 
 const DownloadCV = () => {
   return (
-    <Button onClick={() => window?.open('/files/CV_GiorgioRuiton_25.pdf', '_blank')}>
+    <Button onClick={() => window?.open('/files/CV-GiorgioRuiton-2026.pdf', '_blank')}>
       Descargar CV
     </Button>
   );
