fd4de90a7cfd3e12fd9f1c48c00575903795e826
Giorgio Ruitón Mauricci <gpaoloruitonm@gmail.com>
2026-04-25 15:43:12 -0500

    Changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/dev-portfolio',
+  assetPrefix: '/dev-portfolio/',
+}
 
 module.exports = nextConfig
